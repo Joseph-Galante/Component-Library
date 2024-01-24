3308a2b00a71a486daaf3057bd08033de4fe8e00
Joe Galante <jgalante00@gmail.com>
2024-01-24 13:04:34 -0500

    installed mui and setup main sass modules

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import styles from "./styles.module.scss";
+
+function App() {
+  return (
+    <Box className={styles.app} display="flex" justifyContent="center" mt={2}>
+      Welcome to my component library!
+    </Box>
+  );
+}
+
+export default App;
